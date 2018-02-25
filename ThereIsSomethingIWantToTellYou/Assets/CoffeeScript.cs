5cfa60c6fbfe13f8252167f54502d0ae2cb86668
DESKTOP-9GGN15R\Andres <aceskies@gmail.com>
2018-02-25 13:20:13 +0100

    first version

---
+++ ThereIsSomethingIWantToTellYou/Assets/CoffeeScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoffeeScript : MonoBehaviour
+{
+
+    float wait = 3.0f;
+    bool init = false;
+
+    void Awake()
+    {
+        // You must initialize Dialoguer before using it!
+        Dialoguer.Initialize();
+    }
+
+    void OnGUI()
+    {
+        wait -= Time.deltaTime;
+        if (wait < 0 && !init)
+        {
+            init = true;
+            Dialoguer.StartDialogue(4);
+        }
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+}
